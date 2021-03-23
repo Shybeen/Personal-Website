f1c8c220a639fab2e9fe2d8b7ed9150b19549f8f
shybeen <shaibinktd@gmail.com>
2021-03-23 16:45:26 +0530

    final

--- style.js
+++ style.js
@@ -33,8 +33,81 @@ anime.timeline({loop: true})
     delay: 1000
   });
 
+  function namevalidation(){
+    var text=document.getElementById("name").value;
+    var letters= /^[a-zA-Z\s]*$/;
+  
+    if(text==""){
+      document.getElementById("namerequired").style.display="block";
+      document.getElementById("namelenghth").style.display="none";
+      document.getElementById("nameerror").style.display="none";
+  
+    }else if(!text.match(letters)){
+      document.getElementById("namerequired").style.display="none";
+      document.getElementById("namelenghth").style.display="none";
+      document.getElementById("nameerror").style.display="block";
+  
+    }else if(text.length<4){
+      document.getElementById("namerequired").style.display="none";
+      document.getElementById("namelenghth").style.display="block";
+      document.getElementById("nameerror").style.display="none"; 
+    }else{
+    document.getElementById("namerequired").style.display="none";
+    document.getElementById("namelenghth").style.display="none";
+    document.getElementById("nameerror").style.display="none";
+    }
+  }
+
+  function emailvalidation(){
+    var mail=document.getElementById("email").value;
+    var mailid= /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+  
+    if(mail==""){
+      document.getElementById("emailrequired").style.display="block";
+      document.getElementById("emailcheck").style.display="none";
+      
+  
+  }else if(!mail.match(mailid)){
+    document.getElementById("emailcheck").style.display="block";
+    document.getElementById("emailrequired").style.display="none";
+  
+    
+  }else{
+    document.getElementById("emailrequired").style.display="none";
+    document.getElementById("emailcheck").style.display="none";
+   
+  
+  }
+  
+  
+  }
 
 
+
+  function mobilevalidation(){
+    var mobile=document.getElementById("mobile").value;
+    var mobileid=/[0-9]/;
+  
+    if(mobile==""){
+      document.getElementById("mobilerequired").style.display="block";
+      // document.getElementById("mobilelength").style.display="none";
+      document.getElementById("mobileerror").style.display="none";
+  
+    // }else if(mobile.length<10){
+    //   document.getElementById("mobilerequired").style.display="none";
+    //   document.getElementById("mobilelength").style.display="block";
+    //   document.getElementById("mobileerror").style.display="none";
+    }else if(!mobile.match(mobileid)){
+      document.getElementById("mobilerequired").style.display="none";
+      // document.getElementById("mobilelength").style.display="none";
+      document.getElementById("mobileerror").style.display="block";
+    }else{
+      document.getElementById("mobilerequired").style.display="none";
+      // document.getElementById("mobilelength").style.display="none";
+      document.getElementById("mobileerror").style.display="none";
+    }
+  }
+  
 
 
   // $(document).ready(function(){
